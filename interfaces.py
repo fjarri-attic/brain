97943a2756b41bbbc07c2ab3d51c1fbacd4bda77
Bogdan Opanchuk <bg@bk.ru>
2009-05-04 01:17:50 +0400

    Added ReadRequest
    + __str__'s for interface classes modified a little

--- interfaces.py
+++ interfaces.py
@@ -13,38 +13,6 @@ class Database:
 	def processRequest(self, request):
 		raise Exception("Not implemented")
 
-class _BaseWriteRequest:
-	def __init__(self, id, fields=None):
-		self.id = id
-		self.fields = fields
-
-		if self.fields != None and not isinstance(fields, list):
-			raise FormatError("Data should be list of fields")
-
-class ModifyRequest(_BaseWriteRequest):
-	def __init__(self, id, fields=None):
-		_BaseWriteRequest.__init__(self, id, fields)
-		for field in self.fields:
-			if not isinstance(field, Field):
-				raise FormatError("Field list element must have class Field")
-
-	def __str__(self):
-		if self.fields:
-			fields_list = ":\n" + "\n".join(["\t" + str(field) for field in self.fields])
-		else:
-			fields_list = ""
-
-		return "ModifyRequest for element '" + self.id + "'" + fields_list
-
-class DeleteRequest(_BaseWriteRequest):
-	def __str__(self):
-		if self.fields:
-			fields_list = ":\n" + "\n".join(["\t" + str(field) for field in self.fields])
-		else:
-			fields_list = ""
-
-		return "DeleteRequest for element '" + self.id + "'" + fields_list
-
 class Field:
 	def __init__(self, name, type=None, value=None):
 		self.name = name
@@ -57,9 +25,41 @@ class Field:
 			raise FormatError("Field name must be a list")
 
 	def __str__(self):
-		return "Field '" + str(self.name) + "'" + \
+		return "Field ('" + str(self.name) + "'" + \
 			(", type=" + str(self.type) if self.type else "") + \
-			(", value=" + str(self.value) if self.value else "")
+			(", value=" + str(self.value) if self.value else "") + ")"
+
+class _BaseFieldRelatedRequest:
+	def __init__(self, id, fields=None):
+		self.id = id
+		self.fields = fields
+
+		if self.fields != None and not isinstance(fields, list):
+			raise FormatError("Data should be list of fields")
+
+	def getFieldsStr(self):
+		if self.fields:
+			return ", ".join([str(field) for field in self.fields])
+		else:
+			return ""
+
+class ModifyRequest(_BaseFieldRelatedRequest):
+	def __init__(self, id, fields=None):
+		_BaseFieldRelatedRequest.__init__(self, id, fields)
+		for field in self.fields:
+			if not isinstance(field, Field):
+				raise FormatError("Field list element must have class Field")
+
+	def __str__(self):
+		return "ModifyRequest for element '" + self.id + "': " + self.getFieldsStr()
+
+class DeleteRequest(_BaseFieldRelatedRequest):
+	def __str__(self):
+		return "DeleteRequest for element '" + self.id + "': " + self.getFieldsStr()
+
+class ReadRequest(_BaseFieldRelatedRequest):
+	def __str__(self):
+		return "DeleteRequest for element '" + self.id + "': " + self.__getFieldsStr()
 
 class SearchRequest:
 
