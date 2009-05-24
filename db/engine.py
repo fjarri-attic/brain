110b3de0691873189299155d0d382948da611e57
Bogdan Opanchuk <bg@bk.ru>
2009-05-24 10:37:38 +0400

    db\engine: comments added

--- db/engine.py
+++ db/engine.py
@@ -1,17 +1,21 @@
+"""Module, containing wrappers for different DB engines"""
+
 import sqlite3
 import re
 
 from . import interface
 
 class Sqlite3Engine(interface.Engine):
+	"""Wrapper for Sqlite 3 db engine"""
 
-	__FIELD_SEP = '.'
+	__FIELD_SEP = '.' # separator for field elements in table name
 
 	def __init__(self, path):
 		self.__conn = sqlite3.connect(path)
 		self.__conn.create_function("regexp", 2, self.__regexp)
 
 	def dump(self):
+		"""Dump the whole database to string; used for debug purposes"""
 		print("Dump:")
 		for str in self.__conn.iterdump():
 			print(str)
@@ -39,17 +43,22 @@ class Sqlite3Engine(interface.Engine):
 		self.__conn.execute("DROP TABLE IF EXISTS '" + name + "'")
 
 	def getEmptyCondition(self):
+		"""Returns condition for compound SELECT which evaluates to empty table"""
 		return "SELECT 0 limit 0"
 
 	def getSafeValueFromString(self, s):
+		"""Transform string to something which can be safely used in query as a value"""
 		return "'" + s + "'"
 
 	def getStringFromSafeValue(self, val):
+		"""Transform value back to original string"""
 		return val[1:-1]
 
 	def getSafeTableNameFromList(self, l):
+		"""Transform list of strings to something which can be safely used as a part of table name"""
 		temp_list = [(x if isinstance(x, str) else '') for x in l]
 		return "_" + self.__FIELD_SEP.join(temp_list)
 
 	def getListFromSafeTableName(self, name):
+		"""Transform table name back to original list"""
 		return [(x if x != '' else None) for x in name[1:].split(self.__FIELD_SEP)]
