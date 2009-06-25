d9cc255c36c7f9c94b39dc0761003aeebf3ffa54
Bogdan Opanchuk <bg@bk.ru>
2009-06-26 00:42:09 +0400

    parse\facade: now actually opening and closing session

--- parse/facade.py
+++ parse/facade.py
@@ -2,10 +2,14 @@ import sys, os.path
 scriptdir, scriptfile = os.path.split(sys.argv[0])
 sys.path.append(os.path.join(scriptdir, ".."))
 
-from db import interface, database
+from db import interface, database, engine
 import yaml
 
 class Facade:
+
+	def __init__(self):
+		self.sessions = {}
+		self.session_counter = 0
 
 	def parseYamlRequest(self, request):
 		request = yaml.load(request)
@@ -23,20 +27,40 @@ class Facade:
 
 		return handlers[request['type']](request)
 
-
 	def processOpenRequest(self, request):
-		print("Open request")
+		if not 'name' in request.keys():
+			raise Exception('Database name is missing')
+		name = request['name']
+
+		open_existing = request['open'] if 'open' in request.keys() else None
+
+		self.session_counter += 1
+		self.sessions[self.session_counter] = database.SimpleDatabase(
+			name, engine.Sqlite3Engine)
+
+		return self.session_counter
 
 	def processCloseRequest(self, request):
-		print("Close request")
+		if not 'session' in request.keys():
+			raise Exception('Session ID is missing')
+		session = request['session']
+
+		self.sessions[session] = None
 
 if __name__ == '__main__':
 	f = Facade()
-	f.parseYamlRequest(
+	s = f.parseYamlRequest(
 '''
 type: open
+name: ':memory:'
 ''')
+
+	print("Opened session: " + str(s))
+
 	f.parseYamlRequest(
 '''
 type: close
-''')
+session: {session}
+'''.format(session=s))
+
+	print("Closed session: " + str(s))
