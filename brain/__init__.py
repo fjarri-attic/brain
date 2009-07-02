c824d175642cba930ee321df588c2a6b798a595a
Bogdan Opanchuk <bg@bk.ru>
2009-07-03 00:17:41 +0400

    exporting exceptions in lib level

--- brain/__init__.py
+++ brain/__init__.py
@@ -3,3 +3,4 @@ scriptdir, scriptfile = os.path.split(sys.argv[0])
 sys.path.append(os.path.join(scriptdir, ".."))
 
 from brain.facade import connect
+from brain.interface import StructureError, LogicError, FormatError, FacadeError
