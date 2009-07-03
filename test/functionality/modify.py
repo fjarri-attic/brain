aaa875e80b093887a8fae909008180fecc9e8e36
Bogdan Opanchuk <bg@bk.ru>
2009-07-03 18:50:18 +0400

    test\dunctionality\modify: added tests for blank objects

--- test/functionality/modify.py
+++ test/functionality/modify.py
@@ -6,6 +6,7 @@ import sys, os.path
 scriptdir, scriptfile = os.path.split(sys.argv[0])
 sys.path.append(os.path.join(scriptdir, ".."))
 
+import brain
 from test import helpers
 from test.functionality.requests import TestRequest
 
@@ -14,14 +15,21 @@ class Modify(TestRequest):
 
 	def testBlankObjectAddition(self):
 		"""Check that object without fields cannot be created"""
-		self.failUnlessRaises(Exception, self.conn.create)
-		self.conn.create({})
-		self.conn.create([])
+
+		# Value is the mandatory paramter to this function
+		self.failUnlessRaises(TypeError, self.conn.create)
+
+		self.failUnlessRaises(brain.FacadeError, self.conn.create, {})
+		self.failUnlessRaises(brain.FacadeError, self.conn.create, [])
+		self.failUnlessRaises(brain.FacadeError, self.conn.create, None)
 
 	def testModifyNothing(self):
 		"""Check that modification without parameters does nothing"""
-		obj = self.conn.create({'fld': 1})
+		orig_data = {'fld': 1}
+		obj = self.conn.create(orig_data)
 		self.conn.modify(obj, None)
+		data = self.conn.read(obj)
+		self.failUnlessEqual(data, orig_data)
 
 
 def get_class():
