acbf87c314e85c536dd6c75a065c7a4044e9483e
Bogdan Opanchuk <bg@bk.ru>
2009-07-04 11:39:20 +0400

    test\functionality: removed redundant imports

--- test/functionality/delete.py
+++ test/functionality/delete.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.join(scriptdir, ".."))
 
 import brain
 import brain.op as op
-from test import helpers
 from test.functionality.requests import TestRequest
 
 class Delete(TestRequest):
