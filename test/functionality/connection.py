213b40e96f1cacb9446ad003d4ae0b81966f7e11
Bogdan Opanchuk <bg@bk.ru>
2009-07-05 20:24:44 +0400

    test\functionality\connection: added autocommit test

--- test/functionality/connection.py
+++ test/functionality/connection.py
@@ -12,8 +12,19 @@ from functionality.requests import TestRequest
 class Connection(TestRequest):
 	"""Test different uses of ModifyRequest"""
 
-	def testStub(self):
-		pass
+	def testAutocommitNoErrors(self):
+		"""Check autocommit works when there are no exceptions raised"""
+
+		# we cannot test autocommit on in-memory database
+		if self.db == None: return
+
+		obj = self.conn.create({'name': 'Alex', 'age': 22})
+
+		# now open another connection to this database and check that
+		# changes were really made
+		conn2 = brain.connect(self.db)
+		res = conn2.read(obj)
+		self.assertEqual(res, {'name': 'Alex', 'age': 22})
 
 
 def get_class():
