3c4314ea7c91fee0b509e60358c656fb26631705
Bogdan Opanchuk <bg@bk.ru>
2009-07-11 01:51:05 +0400

    brain: moved getEngineTags() to global exports

--- brain/__init__.py
+++ brain/__init__.py
@@ -5,3 +5,4 @@ sys.path.append(os.path.join(scriptdir, ".."))
 from brain.facade import connect
 from brain.interface import StructureError, LogicError, FormatError, FacadeError
 import brain.op as op
+from brain.engine import getEngineTags
--- test/functionality/requests_all.py
+++ test/functionality/requests_all.py
@@ -7,14 +7,13 @@ scriptdir, scriptfile = os.path.split(sys.argv[0])
 sys.path.append(os.path.join(scriptdir, ".."))
 
 import brain
-from brain.engine import getEngineTags
 import helpers
 from functionality import delete, insert, modify, read, search, requests, connection
 
 def suite():
 	"""Generate test suite for this module"""
 	res = helpers.NamedTestSuite()
-	engine_tags = getEngineTags()
+	engine_tags = brain.getEngineTags()
 
 	parameters = [('memory.' + tag, None, None, tag)
 		for tag in engine_tags]
