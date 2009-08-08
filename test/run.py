8818223992b3d0947dc3e40111ef58f211d8f0bd
Bogdan Opanchuk <bg@bk.ru>
2009-08-08 22:10:05 +0400

    test\run: added modes to run script

--- test/run.py
+++ test/run.py
@@ -5,19 +5,36 @@ import sys
 
 import functionality
 
-# Parse command line arguments
+# Parser settings
 
-parser = optparse.OptionParser(usage = "run.py [options]")
+parser = optparse.OptionParser(usage = "run.py <mode> [options]\n" +
+	"Modes: func, fuzz")
 parser.add_option("--ae", "--all-engines", action="store_true",
-	dest="all_engines", help="run tests using all available DB engines")
+	dest="all_engines", help="[func] run tests using all available DB engines")
 parser.add_option("--ac", "--all-connections", action="store_true",
-	dest="all_connections", help="run tests using all available Connection classes")
+	dest="all_connections", help="[func] run tests using all available Connection classes")
 parser.add_option("--as", "--all-storages", action="store_true",
-	dest="all_storages", help="run tests using all available storages")
+	dest="all_storages", help="[func] run tests using all available storages")
 parser.add_option("-v", "--verbosity", action="store", type="int", default=2,
 	dest="verbosity", help="verbosity level, 0-3")
 
-opts, args = parser.parse_args(sys.argv[1:])
+# Parse options and run tests
 
-functionality.runFunctionalityTests(all_connections=opts.all_connections, all_engines=opts.all_engines,
-    all_storages=opts.all_storages, verbosity=opts.verbosity)
+if len(sys.argv) == 1:
+	parser.error("Error: mode should be specified")
+
+# FIXME: find a way to do it using OptionParser
+modes = ['func', 'fuzz']
+mode = sys.argv[1]
+args = sys.argv[2:]
+
+if mode not in modes:
+	parser.print_help()
+	sys.exit(1)
+
+opts, args = parser.parse_args(args)
+
+if mode == 'func':
+	functionality.runFunctionalityTests(all_connections=opts.all_connections,
+		all_engines=opts.all_engines, all_storages=opts.all_storages,
+		verbosity=opts.verbosity)
