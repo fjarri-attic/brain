e8632cf91c703a4c888a83e0e50f4fa5c32fb3ec
Bogdan Opanchuk <bg@bk.ru>
2009-09-07 22:53:03 +0400

    test\perf: removed all_engines and all_storages, collecting perf results

--- brain/test/perf.py
+++ brain/test/perf.py
@@ -9,7 +9,7 @@ import fuzz
 import brain
 
 
-def runPerformanceTests(all_engines=False, all_storages=False, verbosity=2):
+def runPerformanceTests(verbosity=2):
 	"""Start functionality tests suite"""
 
 	print("Performance tests:")
@@ -17,17 +17,26 @@ def runPerformanceTests(all_engines=False, all_storages=False, verbosity=2):
 	# folder for DBs which are represented by files
 	db_path = tempfile.mkdtemp(prefix='braindb')
 
-	test_suites = public.getEngineTestSuites(db_path, all_engines, all_storages)
+	test_suites = public.getEngineTestSuites(db_path)
 
-	print("* Functionality tests")
 	for test_suite in test_suites:
-		test_time = helpers.TextTestRunner(verbosity=verbosity, 
+		test_time = helpers.TextTestRunner(verbosity=verbosity,
 			show_report=False).run(test_suite)
-		print("- " + test_suite.getTag() + ": {0:.3f} s".format(test_time))
+		print("* Functionality tests: {0:.3f} s".format(test_time))
 
-	for seed in [100, 200, 300]:
+	total_times = None
+	seeds = [100, 200, 300]
+	for seed in seeds:
+		if verbosity > 2:
+			print("Running fuzz test for seed " + str(seed))
 		times = fuzz.runFuzzTest(objects=5, seed=seed, show_report=False)
-		time_strings = ["- " + action + ": {0:.3f} s".format(times[action]) 
-			for action in times]
-		print("* Fuzz test, seed " + str(seed) + 
-			", action times:\n" + "\n".join(time_strings))
+		if total_times is None:
+			total_times = times
+		else:
+			for action in times:
+				total_times[action] += times[action]
+
+	time_strings = ["- " + action + ": {0:.3f} s".format(total_times[action])
+		for action in total_times]
+	print("* Fuzz test, seeds " + ", ".join([str(seed) for seed in seeds]) +
+		", action times:\n" + "\n".join(time_strings))
